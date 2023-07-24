e3a850b9ded8c7ed104baac300fcb9cbf6de1510
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-07-24 15:08:39 -0500

    [ADD] first commit

---
+++ src/main/java/com/debts/debtsappbackend/DebtsAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.debts.debtsappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtsAppBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebtsAppBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/debts/debtsappbackend/DebtsAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debts.debtsappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtsAppBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
